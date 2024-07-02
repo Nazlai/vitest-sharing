303991fcd0b85b87a7801cf13267aa99b43c088c
Joe Ho <joe.he@redso.com.hk>
2024-07-02 16:20:06 +0800

    add precommit hook for linting and pretty print

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "*.{ts,tsx}": [
+    "prettier --write",
+    "eslint",
+    () => "tsc -p tsconfig.json --noemit",
+  ],
+  "*.js": ["prettier --write", "eslint"],
+  "*.html": ["eslint", "prettier --write"],
+  "*.{css,md,json,yaml,yml}": "prettier --write",
+}
